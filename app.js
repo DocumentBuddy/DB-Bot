281baae35b4ddb198e8286f022724cff4ccde68a
LucaVazz <luca.vazzano@gmx.net>
2018-04-21 02:18:26 +0200

    refactoring to claim the code as our own ;)

--- app.js
+++ app.js
@@ -1,63 +1,51 @@
-/*-----------------------------------------------------------------------------
-A simple Language Understanding (LUIS) bot for the Microsoft Bot Framework.
------------------------------------------------------------------------------*/
+var restify = require('restify')
+var builder = require('botbuilder')
+var botbuilder_azure = require("botbuilder-azure")
+var inMemoryStorage = new builder.MemoryBotStorage()
 
-var restify = require('restify');
-var builder = require('botbuilder');
-var botbuilder_azure = require("botbuilder-azure");
 
-// Setup Restify Server
-var server = restify.createServer();
+//--- START CONNECTIONS
+var server = restify.createServer()
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url);
-});
+   console.log('%s now listening to %s', server.name, server.url)
+})
 
-// Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword,
     openIdMetadata: process.env.BotOpenIdMetadata
-});
+})
 
-// Listen for messages from users
-server.post('/api/messages', connector.listen());
+server.post('/api/messages', connector.listen())
 
-/*----------------------------------------------------------------------------------------
-* Bot Storage: This is a great spot to register the private state storage for your bot.
-* We provide adapters for Azure Table, CosmosDb, SQL Azure, or you can implement your own!
-* For samples and documentation, see: https://github.com/Microsoft/BotBuilder-Azure
-* ---------------------------------------------------------------------------------------- */
 
-var tableName = 'botdata';
-var azureTableClient = new botbuilder_azure.AzureTableClient(tableName, process.env['AzureWebJobsStorage']);
-var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azureTableClient);
-
-// Create your bot with a function to receive messages from the user
-// This default message handler is invoked if the user's utterance doesn't
-// match any intents handled by other dialogs.
+//--- INIT BOT
 var bot = new builder.UniversalBot(connector, function (session, args) {
-    session.send('I don\'t understand what you mean with \'%s\', sorry.', session.message.text);
-});
+    // Default Handling: When nothing else matches
+    session.send(
+        '\uD83E\uDD14 I don\'t understand what you mean with \'%s\', sorry.', session.message.text
+    )
+})
+bot.set('storage', inMemoryStorage)
 
-bot.set('storage', tableStorage);
+// extract LUIS settings from environment variables
+var luisAppId = process.env.LuisAppId
+var luisAPIKey = process.env.LuisAPIKey
+var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com'
+const LuisModelUrl =
+    'https://' + luisAPIHostName + '/luis/v2.0/apps/' + luisAppId
+    + '?subscription-key=' + luisAPIKey
 
-// Make sure you add code to validate these fields
-var luisAppId = process.env.LuisAppId;
-var luisAPIKey = process.env.LuisAPIKey;
-var luisAPIHostName = process.env.LuisAPIHostName || 'westus.api.cognitive.microsoft.com';
+// add LUIS recognizer to the bot for reacting to intents
+var recognizer = new builder.LuisRecognizer(LuisModelUrl)
+bot.recognizer(recognizer)
 
-const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v2.0/apps/' + luisAppId + '?subscription-key=' + luisAPIKey;
 
-// Create a recognizer that gets intents from LUIS, and add it to the bot
-var recognizer = new builder.LuisRecognizer(LuisModelUrl);
-bot.recognizer(recognizer);
-
-// Add a dialog for each intent that the LUIS app recognizes.
-// See https://docs.microsoft.com/en-us/bot-framework/nodejs/bot-builder-nodejs-recognize-intent-luis
+//--- INTENT SPECIFIC DIALOG HANDLING
 bot.dialog('GreetingDialog',
     (session) => {
-        session.send('You reached the Greeting intent. You said \'%s\'.', session.message.text);
-        session.endDialog();
+        session.send('Recognized as "Greeting" intent.', session.message.text)
+        session.endDialog()
     }
 ).triggerAction({
     matches: 'Greeting'
@@ -65,8 +53,8 @@ bot.dialog('GreetingDialog',
 
 bot.dialog('HelpDialog',
     (session) => {
-        session.send('You reached the Help intent. You said \'%s\'.', session.message.text);
-        session.endDialog();
+        session.send('Recognized as "Help" intent.', session.message.text)
+        session.endDialog()
     }
 ).triggerAction({
     matches: 'Help'
@@ -74,10 +62,9 @@ bot.dialog('HelpDialog',
 
 bot.dialog('CancelDialog',
     (session) => {
-        session.send('You reached the Cancel intent. You said \'%s\'.', session.message.text);
-        session.endDialog();
+        session.send('Recognized as "Cancel" intent.', session.message.text)
+        session.endDialog()
     }
 ).triggerAction({
     matches: 'Cancel'
 })
-
--- readme.md
+++ readme.md
@@ -1,27 +1,17 @@
-## Use Azure app service editor
+# DcoumentBuddy - Bot Interface
 
-1. make code change in the online editor
+Now with 100% moar NLP!
 
-Your code changes go live as the code changes are saved.
 
-## Use Visual Studio Code
-
-### Build and debug
-1. download source code zip and extract source in local folder
-2. open the source folder in  Visual Studio Code
-3. make code changes
-4. download and run [botframework-emulator](https://emulator.botframework.com/)
-5. connect the emulator to http://localhost:3987
-
-### Publish back
-
+### Setup
 ```
-npm run azure-publish
+npm install
 ```
 
-## Use continuous integration
 
-If you have setup continuous integration, then your bot will automatically deployed when new changes are pushed to the source repository.
-
-
-
+### Run
+```
+set LuisAppID=< placeholder for value from LUIS App Settings >
+set LuisAPIKey=< placeholder for value from LUIS Account Settings, described as Authoring Key >
+node app.js
+```
